8f30e7584bfa7e69280077ca97a77579b10b5128
Krzysztof <krzysztof.hermaszewski.bee@outlook.com>
2021-11-22 23:42:12 +0100

    Add relationships

--- WizLib_DataAccess/Data/ApplicationDbContext.cs
+++ WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -8,16 +8,22 @@ using WizLib_Model.Models;
 
 namespace WizLib_DataAccess.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
-        public DbSet<Category> Categories { get; set; }
+        public DbSet<BookDetail> BookDetails { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookAuthor>().HasKey(ba => new { ba.Author_Id, ba.Book_Id });
+        }
     }
 }
